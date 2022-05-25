2de92d17964d8056a5bfd3e63137efe6eebb0b34
Valentin <7705678@gmail.com>
2022-05-25 22:14:38 +0300

    feat: validation all inputs

--- pages/index.js
+++ pages/index.js
@@ -73,8 +73,10 @@ btnAddNewPlace.addEventListener('click', () => {
   openPopup(newPlacePopup);
 });
 
-newPlacePopupCloseBtn.addEventListener('click', () => {
+newPlacePopupCloseBtn.addEventListener('click', (e) => {
   closePopup(newPlacePopup);
+  const form = e.target.closest('.popup').querySelector('.popup__form');
+  form.reset();
 });
 
 popupImageCloseBtn.addEventListener('click', () => {
@@ -159,3 +161,66 @@ document.addEventListener('keydown', (e) => {
     closePopup(target);
   }
 })
+
+/* Validation Forms */
+
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  console.log(errorElement);
+  inputElement.classList.add('popup__input-field_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input-field_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input-field'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+  console.log(buttonElement);
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach(inputElement => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+function hasInvalidInput(inputList) {
+  return inputList.some(inputElement => {
+    return !inputElement.validity.valid;
+  })
+};
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__submit-button_inactive');
+    buttonElement.setAttribute('disabled','');
+  } else {
+    buttonElement.classList.remove('popup__submit-button_inactive');
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach(formElement => {
+    setEventListeners(formElement);
+  });
+}
+
+enableValidation();
